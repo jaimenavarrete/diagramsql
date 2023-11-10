79db9468cc697f763072dc687a70901184011bed
Jaime Navarrete <jaimenava123@outlook.com>
2023-11-10 16:04:29 -0600

    Add properties styles in table. Add hardcoded properties data in table
    Create table to print properties.
    Add constraint icons.
    Set table max-width and change background colors.

--- src/lib/tables/components/Table.svelte
+++ src/lib/tables/components/Table.svelte
@@ -1,5 +1,10 @@
 <script>
     import { onMount } from 'svelte';
+    import {
+        IconCircleLetterN,
+        IconCircleLetterU,
+        IconKey,
+    } from '@tabler/icons-svelte';
 
     export let table;
     export let canvasHeight;
@@ -43,6 +48,44 @@
     style:left={`${table.positionX}px`}
 >
     <h3>{table.title}</h3>
+    <table class="properties-list">
+        <tr class="property">
+            <td class="key pk-key" title="Primary Key"><IconKey /></td>
+            <td class="name">id</td>
+            <td class="type">int</td>
+            <td class="constraints nullable" />
+            <td class="constraints unique" />
+        </tr>
+        <tr class="property">
+            <td class="key" />
+            <td class="name">name</td>
+            <td class="type">varchar(50)</td>
+            <td class="constraints nullable"
+                ><span title="Nullable"><IconCircleLetterN /></span></td
+            >
+            <td class="constraints unique" />
+        </tr>
+        <tr class="property">
+            <td class="key" title="Foreign Key"><IconKey /></td>
+            <td class="name">categoryId</td>
+            <td class="type">int</td>
+            <td class="constraints nullable" />
+            <td class="constraints unique"
+                ><span title="Unique"><IconCircleLetterU /></span></td
+            >
+        </tr>
+        <tr class="property">
+            <td class="key" />
+            <td class="name">phoneNumber</td>
+            <td class="type">varchar(10)</td>
+            <td class="constraints nullable"
+                ><span title="Nullable"><IconCircleLetterN /></span></td
+            >
+            <td class="constraints unique"
+                ><span title="Unique"><IconCircleLetterU /></span></td
+            >
+        </tr>
+    </table>
     <p>{table.description}</p>
 </div>
 
@@ -53,28 +96,65 @@
 
 <style>
     .table {
-        background-color: #fff;
+        background-color: #ededed;
         border-top: 7px solid var(--table-color);
         border-radius: 5px;
         box-shadow: 0 0 2px var(--table-color);
-        max-width: 300px;
+        color: #4e4e4e;
+        max-width: 450px;
         position: absolute;
         z-index: 2;
     }
 
     h3 {
-        background: #f2f2f2;
-        border-bottom: 1px solid #cbcbcb;
+        border-bottom: 1px solid #e5e5e5;
         cursor: auto;
         font-size: 1.1em;
         padding: 15px;
         text-align: center;
     }
 
-    p {
+    .properties-list {
+        background-color: #fff;
+        border-collapse: collapse;
         cursor: auto;
-        color: #4e4e4e;
+        width: 100%;
+    }
+
+    .properties-list td {
+        font-size: 0.9em;
+        padding: 10px;
+    }
+
+    .properties-list .key {
+        padding: 0 0 0 10px;
+    }
+
+    .properties-list .pk-key {
+        color: #ed991d;
+    }
+
+    .properties-list .name {
+        font-weight: bold;
+        word-break: break-all;
+    }
+
+    .properties-list .constraints {
+        padding: 3px;
+    }
+
+    .properties-list .unique {
+        padding-right: 15px;
+    }
+
+    p {
+        background-color: #fff;
+        border-top: 1px solid #e5e5e5;
+        border-radius: 0 0 5px 5px;
+        cursor: auto;
+        font-size: 0.9em;
+        line-height: 1.8;
         margin: 0;
-        padding: 15px;
+        padding: 15px 10px;
     }
 </style>
