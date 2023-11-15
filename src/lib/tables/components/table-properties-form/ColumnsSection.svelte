97855fe309ea047d4c8d33a78bac6439730d4953
Jaime Navarrete <jaimenava123@outlook.com>
2023-11-15 10:42:07 -0600

    Separate columns section from table properties form
    Import properties section styles.

---
+++ src/lib/tables/components/table-properties-form/ColumnsSection.svelte
@@ -0,0 +1,267 @@
+<script>
+    import {
+        IconCircleLetterC,
+        IconCircleLetterN,
+        IconCircleLetterU,
+        IconColumns,
+        IconColumnsOff,
+        IconKey,
+        IconPlus,
+        IconTrash,
+    } from '@tabler/icons-svelte';
+
+    // Props
+    export let tables;
+    export let selectedTable;
+
+    // Event handlers
+
+    const addColumn = () => {
+        const newColumn = {
+            id: crypto.randomUUID(),
+            isPrimaryKey: false,
+            isNullable: false,
+            isUnique: false,
+        };
+
+        selectedTable.columns = [...selectedTable.columns, newColumn];
+        tables = tables;
+    };
+
+    const deleteColumn = (id) => {
+        selectedTable.columns = selectedTable.columns.filter(
+            (column) => column.id !== id
+        );
+        tables = tables;
+    };
+</script>
+
+<section class="form-section">
+    <h3><IconColumns /> <span>Columns</span></h3>
+
+    {#if !selectedTable?.columns || selectedTable.columns.length === 0}
+        <p class="no-columns-text">
+            <IconColumnsOff size={32} /><span>You have no columns here.</span>
+        </p>
+    {:else}
+        {#each selectedTable.columns as column}
+            <article class="column-control">
+                <input
+                    on:input={(e) => {
+                        column.name = e.currentTarget.value;
+                        tables = tables;
+                    }}
+                    type="text"
+                    placeholder="Name"
+                    value={column.name || null}
+                />
+                <input
+                    on:input={(e) => {
+                        column.type = e.currentTarget.value;
+                        tables = tables;
+                    }}
+                    type="text"
+                    id="column-type"
+                    placeholder="Type"
+                    value={column.type || null}
+                />
+                <label class="checkbox is-primary-key" title="Primary Key">
+                    <input
+                        on:change={(e) => {
+                            column.isPrimaryKey = e.currentTarget.checked;
+                            tables = tables;
+                        }}
+                        type="checkbox"
+                        value={column.isPrimaryKey}
+                    />
+                    <button type="button"><IconKey /></button>
+                </label>
+
+                <!-- Show constraints button -->
+                <label class="checkbox constraints-button" title="Constraints">
+                    <input type="checkbox" />
+                    <button type="button"><IconCircleLetterC /></button>
+
+                    <div class="constraints-container">
+                        <label class="checkbox is-nullable" title="Nullable">
+                            <input
+                                on:change={(e) => {
+                                    column.isNullable = e.currentTarget.checked;
+                                    tables = tables;
+                                }}
+                                type="checkbox"
+                                value={column.isNullable}
+                            />
+                            <button type="button"><IconCircleLetterN /></button>
+                        </label>
+                        <label class="checkbox is-unique" title="Unique">
+                            <input
+                                on:change={(e) => {
+                                    column.isUnique = e.currentTarget.checked;
+                                    tables = tables;
+                                }}
+                                type="checkbox"
+                                value={column.isUnique}
+                            />
+                            <button type="button"><IconCircleLetterU /></button>
+                        </label>
+                    </div>
+                </label>
+
+                <!-- Delete button -->
+                <div
+                    class="checkbox delete-column-button"
+                    title="Delete column"
+                >
+                    <button on:click={() => deleteColumn(column.id)}
+                        ><IconTrash /></button
+                    >
+                </div>
+            </article>
+        {/each}
+    {/if}
+
+    <div class="form-control add-column-button">
+        <span>N° of columns: {selectedTable?.columns.length}</span>
+        <button on:click={addColumn}><IconPlus size={22} /> Add column</button>
+    </div>
+</section>
+
+<style>
+    input {
+        background: #f2f2f2;
+        border: 1px solid #e4e4e4;
+        border-radius: 5px;
+        width: 100%;
+    }
+
+    input:focus {
+        outline-color: #4752c7;
+    }
+
+    button {
+        background: transparent;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+        padding: 10px 20px;
+    }
+
+    .add-column-button {
+        border-top: 1px solid #f2f2f2;
+        margin-top: 20px;
+        padding-top: 5px;
+
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .add-column-button span {
+        color: #4e4e4e;
+        font-size: 14px;
+    }
+
+    .add-column-button button {
+        border: 2px dashed transparent;
+        color: #5865f2;
+        font-size: 15px;
+
+        font-weight: bold;
+        padding: 7px 15px;
+        transition: all 0.1s ease-out;
+    }
+
+    .add-column-button button:hover {
+        background-color: #4752c720;
+        border-color: #4752c7;
+    }
+
+    /* Empty columns text */
+    .no-columns-text {
+        color: #4e4e4e;
+        font-size: 0.9em;
+        text-align: center;
+    }
+
+    .no-columns-text span {
+        display: block;
+    }
+
+    /*** COLUMNS STYLES ***/
+
+    .column-control {
+        margin-bottom: 7px;
+
+        display: flex;
+    }
+
+    .column-control input {
+        font-size: 14px;
+        margin: 0 2px;
+        padding: 0 10px;
+    }
+
+    .column-control .checkbox {
+        cursor: pointer;
+    }
+
+    .column-control .checkbox input {
+        display: none;
+    }
+
+    .column-control .checkbox button {
+        color: #e4e4e4;
+        padding: 10px;
+    }
+
+    .column-control .checkbox:hover > button {
+        background: #f2f2f2;
+        color: #4e4e4e;
+        pointer-events: none;
+    }
+
+    .column-control .is-primary-key input:checked ~ button {
+        color: #ed991d;
+    }
+
+    .column-control .is-nullable input:checked ~ button,
+    .column-control .is-unique input:checked ~ button {
+        color: #5865f2;
+    }
+
+    .column-control .constraints-button {
+        position: relative;
+    }
+
+    .column-control .constraints-container {
+        background: #fff;
+        border-radius: 5px;
+        box-shadow: 0 0 15px #21212140;
+        display: none;
+        position: absolute;
+        z-index: 1;
+    }
+
+    .column-control
+        .constraints-button
+        > input:checked
+        ~ .constraints-container {
+        display: block;
+    }
+
+    .column-control .constraints-button > input:checked ~ button {
+        background: #f2f2f2;
+        color: #4e4e4e;
+    }
+
+    .column-control .delete-column-button button {
+        color: #db3b21;
+    }
+
+    .column-control .delete-column-button:hover button {
+        background: #db3b2120;
+        color: #db3b21;
+        pointer-events: initial;
+    }
+</style>
--- src/lib/tables/components/table-properties-form/TablePropertiesForm.svelte
+++ src/lib/tables/components/table-properties-form/TablePropertiesForm.svelte
@@ -1,19 +1,11 @@
 <script>
-    import { fade } from 'svelte/transition';
+    import './../../../../assets/css/properties-form-section.css';
 
-    import {
-        IconCircleLetterC,
-        IconCircleLetterN,
-        IconCircleLetterU,
-        IconColumns,
-        IconColumnsOff,
-        IconInfoCircle,
-        IconKey,
-        IconPlus,
-        IconTrash,
-        IconX,
-    } from '@tabler/icons-svelte';
     import { onMount } from 'svelte';
+    import { fade } from 'svelte/transition';
+    import { IconInfoCircle, IconX } from '@tabler/icons-svelte';
+
+    import ColumnsSection from './ColumnsSection.svelte';
 
     export let tables;
     export let selectedTable;
@@ -30,25 +22,6 @@
     $: selectedTableId, focusedElementRef?.focus();
 
     // Event handlers
-
-    const addColumn = () => {
-        const newColumn = {
-            id: crypto.randomUUID(),
-            isPrimaryKey: false,
-            isNullable: false,
-            isUnique: false,
-        };
-
-        selectedTable.columns = [...selectedTable.columns, newColumn];
-        tables = tables;
-    };
-
-    const deleteColumn = (id) => {
-        selectedTable.columns = selectedTable.columns.filter(
-            (column) => column.id !== id
-        );
-        tables = tables;
-    };
 
     const resizeForm = (e) => {
         if (!isResizeBarGrabbed) return;
@@ -119,112 +92,7 @@
             />
         </div>
     </section>
-    <section class="form-section">
-        <h3><IconColumns /> <span>Columns</span></h3>
-
-        {#if !selectedTable?.columns || selectedTable.columns.length === 0}
-            <p class="no-columns-text">
-                <IconColumnsOff size={32} /><span
-                    >You have no columns here.</span
-                >
-            </p>
-        {:else}
-            {#each selectedTable.columns as column}
-                <article class="column-control">
-                    <input
-                        on:input={(e) => {
-                            column.name = e.currentTarget.value;
-                            tables = tables;
-                        }}
-                        type="text"
-                        placeholder="Name"
-                        value={column.name || null}
-                    />
-                    <input
-                        on:input={(e) => {
-                            column.type = e.currentTarget.value;
-                            tables = tables;
-                        }}
-                        type="text"
-                        id="column-type"
-                        placeholder="Type"
-                        value={column.type || null}
-                    />
-                    <label class="checkbox is-primary-key" title="Primary Key">
-                        <input
-                            on:change={(e) => {
-                                column.isPrimaryKey = e.currentTarget.checked;
-                                tables = tables;
-                            }}
-                            type="checkbox"
-                            value={column.isPrimaryKey}
-                        />
-                        <button type="button"><IconKey /></button>
-                    </label>
-
-                    <!-- Show constraints button -->
-                    <label
-                        class="checkbox constraints-button"
-                        title="Constraints"
-                    >
-                        <input type="checkbox" />
-                        <button type="button"><IconCircleLetterC /></button>
-
-                        <div class="constraints-container">
-                            <label
-                                class="checkbox is-nullable"
-                                title="Nullable"
-                            >
-                                <input
-                                    on:change={(e) => {
-                                        column.isNullable =
-                                            e.currentTarget.checked;
-                                        tables = tables;
-                                    }}
-                                    type="checkbox"
-                                    value={column.isNullable}
-                                />
-                                <button type="button"
-                                    ><IconCircleLetterN /></button
-                                >
-                            </label>
-                            <label class="checkbox is-unique" title="Unique">
-                                <input
-                                    on:change={(e) => {
-                                        column.isUnique =
-                                            e.currentTarget.checked;
-                                        tables = tables;
-                                    }}
-                                    type="checkbox"
-                                    value={column.isUnique}
-                                />
-                                <button type="button"
-                                    ><IconCircleLetterU /></button
-                                >
-                            </label>
-                        </div>
-                    </label>
-
-                    <!-- Delete button -->
-                    <div
-                        class="checkbox delete-column-button"
-                        title="Delete column"
-                    >
-                        <button on:click={() => deleteColumn(column.id)}
-                            ><IconTrash /></button
-                        >
-                    </div>
-                </article>
-            {/each}
-        {/if}
-
-        <div class="form-control add-column-container">
-            <span>N° of columns: {selectedTable?.columns.length}</span>
-            <button on:click={addColumn}
-                ><IconPlus size={22} /> Add column</button
-            >
-        </div>
-    </section>
+    <ColumnsSection bind:tables bind:selectedTable />
 
     <!-- svelte-ignore a11y-no-static-element-interactions -->
     <div on:mousedown={() => (isResizeBarGrabbed = true)} class="resize-bar" />
@@ -264,25 +132,6 @@
         font-size: 1em;
         padding: 10px 20px;
         text-transform: uppercase;
-    }
-
-    .form-section {
-        margin-bottom: 30px;
-        padding: 0px 20px;
-    }
-
-    .form-section h3 {
-        border-bottom: 1px solid #f2f2f2;
-        font-size: 1em;
-        margin-bottom: 20px;
-        padding-bottom: 10px;
-
-        display: flex;
-        align-items: center;
-    }
-
-    .form-section h3 span {
-        margin-left: 5px;
     }
 
     form label {
@@ -331,124 +180,6 @@
 
     form button:hover {
         color: #5865f2;
-    }
-
-    form .add-column-container {
-        border-top: 1px solid #f2f2f2;
-        margin-top: 20px;
-        padding-top: 5px;
-
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-    }
-
-    form .add-column-container span {
-        color: #4e4e4e;
-        font-size: 14px;
-    }
-
-    form .add-column-container button {
-        border: 2px dashed transparent;
-        color: #5865f2;
-        font-size: 15px;
-
-        font-weight: bold;
-        padding: 7px 15px;
-        transition: all 0.1s ease-out;
-    }
-
-    form .add-column-container button:hover {
-        background-color: #4752c720;
-        border-color: #4752c7;
-    }
-
-    /*** COLUMNS STYLES ***/
-
-    .no-columns-text {
-        color: #4e4e4e;
-        font-size: 0.9em;
-        text-align: center;
-    }
-
-    .no-columns-text span {
-        display: block;
-    }
-
-    .column-control {
-        margin-bottom: 7px;
-
-        display: flex;
-    }
-
-    .column-control input {
-        font-size: 14px;
-        margin: 0 2px;
-        padding: 0 10px;
-    }
-
-    .column-control .checkbox {
-        cursor: pointer;
-        margin-bottom: 0;
-    }
-
-    .column-control .checkbox input {
-        display: none;
-    }
-
-    .column-control .checkbox button {
-        color: #e4e4e4;
-        padding: 10px;
-    }
-
-    .column-control .checkbox:hover > button {
-        background: #f2f2f2;
-        color: #4e4e4e;
-        pointer-events: none;
-    }
-
-    .column-control .is-primary-key input:checked ~ button {
-        color: #ed991d;
-    }
-
-    .column-control .is-nullable input:checked ~ button,
-    .column-control .is-unique input:checked ~ button {
-        color: #5865f2;
-    }
-
-    .column-control .constraints-button {
-        position: relative;
-    }
-
-    .column-control .constraints-container {
-        background: #fff;
-        border-radius: 5px;
-        box-shadow: 0 0 15px #21212140;
-        display: none;
-        position: absolute;
-        z-index: 1;
-    }
-
-    .column-control
-        .constraints-button
-        > input:checked
-        ~ .constraints-container {
-        display: block;
-    }
-
-    .column-control .constraints-button > input:checked ~ button {
-        background: #f2f2f2;
-        color: #4e4e4e;
-    }
-
-    .column-control .delete-column-button button {
-        color: #db3b21;
-    }
-
-    .column-control .delete-column-button:hover button {
-        background: #db3b2120;
-        color: #db3b21;
-        pointer-events: initial;
     }
 
     /*** RESIZE BAR STYLES ***/
