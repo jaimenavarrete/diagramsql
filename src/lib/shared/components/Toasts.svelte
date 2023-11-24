727775e7794558a70d8b9a668c5888d8837a7d9a
Jaime Navarrete <jaimenava123@outlook.com>
2023-11-23 23:59:09 -0600

    Add default data to toast notifications. Change notifications position
    Add style, position and duration default data.
    Change notifications position from top center to top right.

--- src/lib/shared/components/Toasts.svelte
+++ src/lib/shared/components/Toasts.svelte
@@ -1,60 +1,69 @@
 <script context="module">
     import toast, { Toaster } from 'svelte-french-toast';
 
+    const defaultData = {
+        getStyle: (color) =>
+            `background: ${color}; color: #FFFFFF; font-weight: bold; padding: 16px`,
+        position: /** @type {import("svelte-french-toast").ToastPosition} */ (
+            'top-right'
+        ),
+        duration: 3500,
+    };
+
     const defaultToast = (message) => {
         toast(message, {
-            position: 'top-center',
-            duration: 3500,
+            position: defaultData.position,
+            duration: defaultData.duration,
         });
     };
 
     const successToast = (message) => {
         toast.success(message, {
-            style: 'background: #29B564; color: #FFFFFF; font-weight: bold; padding: 16px',
+            style: defaultData.getStyle('#29B564'),
             iconTheme: {
                 primary: '#FFFAEE',
                 secondary: '#29B564',
             },
-            position: 'top-center',
-            duration: 3500,
+            position: defaultData.position,
+            duration: defaultData.duration,
         });
     };
 
     const errorToast = (message) => {
         toast.error(message, {
-            style: 'background: #E14D45; color: #FFFFFF; font-weight: bold; padding: 16px',
+            style: defaultData.getStyle('#E14D45'),
             iconTheme: {
                 primary: '#FFFAEE',
                 secondary: '#E14D45',
             },
-            position: 'top-center',
-            duration: 3500,
+            position: defaultData.position,
+            duration: defaultData.duration,
         });
     };
 
     const warningToast = (message) => {
         toast(message, {
             icon: '⚠️',
-            style: 'background: #ED991D; color: #FFFFFF; font-weight: bold; padding: 16px',
+            style: defaultData.getStyle('#ED991D'),
             iconTheme: {
                 primary: '#FFFAEE',
                 secondary: '#ED991D',
             },
-            position: 'top-center',
-            duration: 3500,
+            position: defaultData.position,
+            duration: defaultData.duration,
         });
     };
 
     const infoToast = (message) => {
         toast(message, {
             icon: 'ℹ️',
-            style: 'background: #3C90D1; color: #FFFFFF; font-weight: bold; padding: 16px',
+            style: defaultData.getStyle('#3C90D1'),
             iconTheme: {
                 primary: '#FFFAEE',
                 secondary: '#3C90D1',
             },
-            position: 'top-center',
-            duration: 3500,
+            position: defaultData.position,
+            duration: defaultData.duration,
         });
     };
 
