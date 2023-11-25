5ab7924d4136106625601f9f4072b1e89fb52d65
Jaime Navarrete <jaimenava123@outlook.com>
2023-11-25 16:35:19 -0600

    Create navigation menu in diagram area sidebar
    Add recent diagrams, favorite diagrams and all diagrams buttons.
    Add folders section and add hardcoded folders.

--- src/lib/diagrams/components/DiagramListSidebar.svelte
+++ src/lib/diagrams/components/DiagramListSidebar.svelte
@@ -1,4 +1,12 @@
 <script>
+    import {
+        IconClock,
+        IconFolder,
+        IconFolders,
+        IconHeart,
+        IconLayoutGrid,
+    } from '@tabler/icons-svelte';
+
     // Element references
     let formRef;
 
@@ -16,8 +24,41 @@
 
 <aside bind:this={formRef} style:width={`${formWidth}px`}>
     <!-- Content -->
-    <nav class="form-section menu-buttons">
-        <ul class="menu-items"></ul>
+    <nav class="form-section nav-menu">
+        <ul class="menu-items">
+            <li>
+                <button class="menu-button"
+                    ><IconClock size={20} /> <span>Recent diagrams</span>
+                </button>
+            </li>
+            <li>
+                <button class="menu-button"
+                    ><IconHeart size={20} /> <span>Favorite diagrams</span>
+                </button>
+            </li>
+        </ul>
+        <ul class="menu-items">
+            <li>
+                <button class="menu-button is-active"
+                    ><IconLayoutGrid size={20} /> <span>All diagrams</span>
+                </button>
+            </li>
+        </ul>
+
+        <h3><IconFolders /> <span>Folders</span></h3>
+
+        <ul class="menu-items">
+            <li>
+                <button class="menu-button"
+                    ><IconFolder size={20} /> <span>My folder</span>
+                </button>
+            </li>
+            <li>
+                <button class="menu-button"
+                    ><IconFolder size={20} /> <span>Public diagrams</span>
+                </button>
+            </li>
+        </ul>
     </nav>
 
     <!-- svelte-ignore a11y-no-static-element-interactions -->
@@ -37,14 +78,46 @@
 
     aside {
         background: #fff;
-        border-right: 1px solid #e4e4e4;
+        border-right: 1px solid #f2f2f2;
         overflow: auto;
+        padding-top: 30px;
         padding-bottom: 100px;
         position: relative;
     }
 
-    .menu-items {
+    .form-section h3 {
+        margin-top: 30px;
+        font-weight: bold;
+    }
+
+    .nav-menu .menu-items {
         list-style-type: none;
+        padding-bottom: 30px;
+    }
+
+    .nav-menu .menu-button {
+        background: none;
+        border: 1px solid transparent;
+        border-radius: 5px;
+        cursor: pointer;
+        font-size: 0.95em;
+        padding: 10px;
+        transition: all 0.2s ease-out;
+        text-align: left;
+        width: 100%;
+
+        justify-content: start;
+        align-items: start;
+    }
+
+    .nav-menu .menu-button:hover,
+    .nav-menu .menu-button.is-active {
+        background-color: #5865f220;
+        color: #4752c7;
+    }
+
+    .nav-menu .menu-button span {
+        margin-left: 5px;
     }
 
     /*** RESIZE BAR STYLES ***/
