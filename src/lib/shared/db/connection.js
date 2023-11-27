1cf0a4f70f2d61e4652321e3cbce651373d864fa
Jaime Navarrete <jaimenava123@outlook.com>
2023-11-27 15:15:17 -0600

    Implement client side database IndexedDB
    Create connection file to open database and create diagrams object
    store.

---
+++ src/lib/shared/db/connection.js
@@ -0,0 +1,37 @@
+let db;
+
+const createDiagramsObjectStore = (dbCreated) => {
+    const objectStore = dbCreated.createObjectStore('diagrams', {
+        keyPath: 'id',
+    });
+
+    objectStore.createIndex('title', 'title', { unique: true });
+    objectStore.createIndex('description', 'description', { unique: false });
+    objectStore.createIndex('isFavorite', 'isFavorite', { unique: false });
+    objectStore.createIndex('tables', 'description', { unique: false });
+    objectStore.createIndex('notes', 'description', { unique: false });
+};
+
+const openDB = () => {
+    return new Promise((resolve, reject) => {
+        const DBOpenRequest = window.indexedDB.open('diagramsql', 4);
+
+        DBOpenRequest.onupgradeneeded = (event) => {
+            // @ts-ignore
+            db = event.target.result;
+            createDiagramsObjectStore(db);
+            console.log('Creados');
+        };
+
+        DBOpenRequest.onsuccess = () => resolve(DBOpenRequest.result);
+        DBOpenRequest.onerror = () => reject();
+    });
+};
+
+try {
+    db = await openDB();
+} catch (error) {
+    console.log(error);
+}
+
+export { db };
